5f563c6c252abc2d3d4c44304176a3a0c7edeeea
NJ <njadhavmh23@gmail.com>
2026-08-22 13:51:02 +0530

    Initial commit

---
+++ DynamoDB/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 1.88"
+    }
+  }
+}
+
+provider "awscc" {
+  region = "ap-south-1"
+}
+
+variable "connect_instance_arn" {
+  description = "Amazon Connect Instance ARN"
+  type        = string
+}
+
+resource "awscc_connect_data_table" "customer_data" {
+  instance_arn = var.connect_instance_arn
+
+  name             = "CustomerData"
+  description      = "Customer data table for Amazon Connect"
+  status           = "PUBLISHED"
+  time_zone        = "Asia/Kolkata"
+  value_lock_level = "NONE"
+
+  tags = [
+    {
+      key   = "Environment"
+      value = "dev"
+    },
+    {
+      key   = "Project"
+      value = "amazon-connect"
+    }
+  ]
+}
+
+output "data_table_arn" {
+  value = awscc_connect_data_table.customer_data.arn
+}
+
+output "data_table_id" {
+  value = awscc_connect_data_table.customer_data.id
+}
